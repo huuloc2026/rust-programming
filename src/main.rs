04769abf23af13d76eb72fbd33c253690d329c0e
huuloc2026 <huuloc2026@gmail.com>
2024-11-01 23:57:36 +0700

    refresh options

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-fn main(){
-    let x = 2;
-    let square = |i| -> i32 {
-        i*i
-    }(x);
-    println!("{}", square);
+fn main() {
+   let _value = Some(4);
+    match _value {
+        Some(5) => println!("bang 5"),
+        _ => println!("khac 5")
+    }
 }
--- src/options.rs
+++ src/options.rs
@@ -1,7 +1,33 @@
 
 fn main(){
-    let x: i32 = 5;
-    let y: Option<i32> = Some(5);
-    let sum = x + y.unwrap_or(1);
-    println!("sum = {}",sum)
+     //    let _number = Some(5);
+    //    let _string = Some("A String");
+    //    let _nonenumber: Option<i32> = None;
+    let x = 5;
+    let y = None;
+    let sum = x + y.unwrap_or(2);
+    println!("sum is {}",sum)
 }
+
+fn main() {
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("six is {:#?}",six);
+    let none = plus_one(None);
+    println!("None is = {:#?}",none);
+}
+fn plus_one(x: Option<i32>) -> Option<i32>{
+    match x {
+        Some(x) => Some(x+1),
+        _ => None,
+    }
+}
+
+fn main() {
+    let _value = Some(4);
+     match _value {
+         Some(5) => println!("bang 5"),
+         _ => println!("khac 5")
+     }
+ }
+ 
