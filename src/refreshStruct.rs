9e588baedc9f77e3152e00a9ee5c370f442692e7
huuloc2026 <huuloc2026@gmail.com>
2024-11-02 18:28:56 +0700

    error handling

--- src/refreshStruct.rs
+++ src/refreshStruct.rs
@@ -13,4 +13,28 @@ fn main() {
     println!("Infor of Alice is {:?}",alice)
 
 }
+#[derive(Debug)]
+struct Student{
+    _name:String,
+    _age:u8,
+    _class:String,
+}
+struct Macbook;
+impl Macbook {
+    pub fn get_name(&self) -> String {
+        "M1".to_string()
+    } 
+}
+fn main() {
+    // let alice = Student {
+    //     _name: String::from("Alice"),
+    //     _age: 20,
+    //     _class:String::from("Rust Bootcamp"),
+    // };
+    // println!("Infor of Alice is {:?}",alice);
+    // println!("age of Alice is {}",alice._age);
+    let m1 = Macbook{};
+    println!("Name of Macbook is {}",m1.get_name());
+}
+
 
